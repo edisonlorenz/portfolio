593173377318af7c8d5a8d334f7f1ad662f6cd60
edisonlorenz <edisonlorenz84@gmail.com>
2021-06-10 01:11:10 +0800

    New layout  showcase

--- js/index.js
+++ js/index.js
@@ -1,39 +1,34 @@
-const openSideMenu = document.getElementById("open-sidebar");
-const sidebar = document.getElementById("sidebar");
-const menuIndicator = document.querySelector(".fas");
+const nav = document.querySelector("#navbar");
 
-let isOpen = true;
-const navLinks = document.querySelector(".nav-links");
+//Handle Nav  Hover
+const handleHover = function (e) {
+  if (e.target.classList.contains("nav-link")) {
+    const link = e.target;
+    const siblings = link.closest("#navbar").querySelectorAll(".nav-link");
+    const logo = link.closest("#navbar").querySelector(".logo");
 
-const sidebarStyle = (position, transition, remove) => {
-  sidebar.style.left = position;
-  sidebar.style.transition = transition;
-  menuIndicator.classList.remove("fa-bars");
-  menuIndicator.classList.add("fa-times");
-  if (remove !== "remove") {
-    menuIndicator.classList.add("fa-bars");
-    menuIndicator.classList.remove("fa-times");
+    siblings.forEach((el) => {
+      if (el !== link) {
+        el.style.opacity = this;
+      }
+    });
+    logo.style.opacity = this;
   }
 };
-openSideMenu.addEventListener("click", function (e) {
-  if (isOpen) {
-    sidebarStyle(0, "all ease .5s", "remove");
-  } else {
-    sidebarStyle("-250px", "all ease .5s");
-  }
-  isOpen = !isOpen;
-});
 
-//Navigation bar
-navLinks.addEventListener("click", function (e) {
-  if (e.target.classList.contains("nav-link")) {
-    e.preventDefault();
-    sidebarStyle("-250px", "none");
-    const id = e.target.getAttribute("href");
-    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
-  }
-});
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if (this.scrollY > 20) {
+      $("#navbar").addClass("sticky");
+    } else {
+      $("#navbar").removeClass("sticky");
+    }
+  });
+  $("#navbar").on("mouseout", handleHover.bind(1));
+  $("#navbar").on("mouseover", handleHover.bind(0.5));
 
-window.addEventListener("scroll", () => {
-  sidebarStyle("-250px", "none");
+  $(".menu-btn").click(function () {
+    $("#navbar #nav-links").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
+  });
 });
